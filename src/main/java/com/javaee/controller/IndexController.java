dc5549b9e7cabfa7d1c5cc793ef5f110fff7363e
Asif Jaman <asifuzzaman.opi@gmail.com>
2025-02-09 19:19:02 +0600

    list of countries with pagination

--- src/main/java/com/javaee/controller/IndexController.java
+++ src/main/java/com/javaee/controller/IndexController.java
@@ -1,5 +1,10 @@
 package com.javaee.controller;
 
+import com.javaee.dao.CountryDAO;
+import com.javaee.dao.CountryDAOImpl;
+import com.javaee.entity.Country;
+import com.javaee.service.CountryService;
+import com.javaee.service.CountryServiceImpl;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.Serial;
+import java.util.List;
 
 @WebServlet({"/", "/home", "/index"})
 public class IndexController extends HttpServlet {
@@ -18,7 +24,41 @@ public class IndexController extends HttpServlet {
 
     public IndexController() {
     }
+
+    private CountryService countryService;
+
+    @Override
+    public void init() throws ServletException {
+        CountryDAO countryDAO = new CountryDAOImpl();
+        this.countryService = new CountryServiceImpl(countryDAO);
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int page = 1;
+
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+
+        try {
+            int recordsPerPage = 5;
+            int offset = (page - 1) * recordsPerPage;
+
+            List<Country> countries = countryService.findAll(offset, recordsPerPage);
+
+            int totalCountries = countryService.count();
+
+            int totalPages = (int) Math.ceil((double) totalCountries/recordsPerPage);
+
+            request.setAttribute("countries", countries);
+            request.setAttribute("totalPages", totalPages);
+            request.setAttribute("currentPage", page);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        request.setAttribute("home", "Home Page");
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/index.jsp");
         dispatcher.forward(request, response);
     }
---
+++ src/main/webapp/pages/partials/navigation.jsp
@@ -0,0 +1,25 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="path" value="${pageContext.request.contextPath}"/>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<c:url value="/" />">Java EE,Spring and Cloud Training</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="<c:url value="/save" />">Create</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
